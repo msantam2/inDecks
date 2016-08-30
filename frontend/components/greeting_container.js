e26e91964a943454f687e588660119afbe73df24
Matthew Santamaria <msantam2@gmail.com>
2016-08-30 16:58:37 -0700

    create components: Greeting, GreetingContainer, App, Root

---
+++ frontend/components/greeting_container.js
@@ -0,0 +1,13 @@
+import Greeting from './greeting';
+import { SessionActions } from '../actions/session_actions';
+import { connect } from 'react-redux';
+
+const mapStateToProps = state => ({
+  currentUser: state.session.currentUser
+});
+
+const mapDispatchToProps = dispatch => ({
+  logout: () => dispatch(SessionActions.logout())
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Greeting);
--- frontend/indecks.jsx
+++ frontend/indecks.jsx
@@ -3,9 +3,14 @@ import ReactDOM from 'react-dom';
 import SessionApiUtil from './util/session_api_util';
 import { SessionActions } from './actions/session_actions';
 import SessionMiddleware from './middleware/session_middleware';
+import SessionReducer from './reducers/session_reducer';
+import Root from './components/root';
+
+window.SessionApiUtil = SessionApiUtil;
+window.SessionActions = SessionActions;
+window.SessionMiddleware = SessionMiddleware;
 
 document.addEventListener("DOMContentLoaded", () => {
-  window.SessionApiUtil = SessionApiUtil;
-  window.SessionActions = SessionActions;
-  window.SessionMiddleware = SessionMiddleware; 
+  const root = document.getElementById('root');
+  // ReactDOM.render(<Root store={store} />, root);
 });

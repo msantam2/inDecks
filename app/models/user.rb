cb3176e188d2eca5cc89be8eb014896b13810a6c
Matthew Santamaria <msantam2@gmail.com>
2016-08-30 11:31:25 -0700

    populate User model with validations, session token methods, and find_by_credentials method

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,5 +11,41 @@
 #
 
 class User < ActiveRecord::Base
-  
+  before_validation :ensure_session_token
+
+  validates :email, :password_digest, :session_token, pressence: true
+  validates :password, length: { minimum: 8, allow_nil: true }
+
+  attr_reader :password
+
+  def self.find_by_credentials(email, password)
+    user = User.find_by(email: email)
+    return user if user && user.is_password?(password)
+    nil 
+  end
+
+  def self.generate_session_token
+    SecureRandom.urlsafe_base64(16)
+  end
+
+  def reset_session_token!
+    self.session_token = User.generate_session_token
+    self.save!
+    self.session_token
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  private
+
+  def ensure_session_token
+    self.session_token ||= User.generate_session_token
+  end
 end

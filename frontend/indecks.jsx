389f420332eb6846cdcb7662abea35aa8dba74ba
Matthew Santamaria <msantam2@gmail.com>
2016-08-30 17:17:28 -0700

    create RootReducer and RootMiddleware. create configureStore function in frontend/store/store.js that uses RootReducer and Rootmiddleware to create a store

--- frontend/indecks.jsx
+++ frontend/indecks.jsx
@@ -5,12 +5,14 @@ import { SessionActions } from './actions/session_actions';
 import SessionMiddleware from './middleware/session_middleware';
 import SessionReducer from './reducers/session_reducer';
 import Root from './components/root';
-
-window.SessionApiUtil = SessionApiUtil;
-window.SessionActions = SessionActions;
-window.SessionMiddleware = SessionMiddleware;
+import configureStore from './store/store';
+// window.SessionApiUtil = SessionApiUtil;
+// window.SessionActions = SessionActions;
+// window.SessionMiddleware = SessionMiddleware;
 
 document.addEventListener("DOMContentLoaded", () => {
+  const store = configureStore();
   const root = document.getElementById('root');
-  // ReactDOM.render(<Root store={store} />, root);
+  window.store = store; 
+  ReactDOM.render(<Root store={store} />, root);
 });

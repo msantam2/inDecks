3841a53b1d8a4784e9fc8c8ab5741bffa435b99c
Matthew Santamaria <msantam2@gmail.com>
2016-09-01 10:39:02 -0700

    seed users

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create(email: 'timyates@gmail.com', password: 'timyates')
+User.create(email: 'markthompson@gmail.com', password: 'markthompson')
+User.create(email: 'johnhamlin@gmail.com', password: 'johnhamlin')
+User.create(email: 'brookejones@gmail.com', password: 'brookejones')
+User.create(email: 'alisonchristine@gmail.com', password: 'alisonchristine')
--- frontend/components/greeting/greeting.jsx
+++ frontend/components/greeting/greeting.jsx
@@ -3,19 +3,14 @@ import { Link } from 'react-router';
 
 const sessionLinks = () => (
   <div className='splash'>
-    <div className='background-img'></div>
+    <div className='index-card'>
+      <p className='tagline'>inDecks: A more <span className='underline'>fun</span> and <span className='underline'>efficient</span> way to study.</p>
+    </div>
+
     <div className='signup-login'>
       <button className='sign-up-btn'><Link to='/signup' className='sign-up-link' activeClassName='current'>Sign Up</Link></button>
 
       <button className='login-btn'><Link to='/login' className='login-link' activeClassName='current'>Login</Link></button>
-    </div>
-
-    <div className='index-card-container'>
-      <div class='index-card'>
-        <img className='index-card-img' src='https://upload.wikimedia.org/wikipedia/commons/2/2e/Notecard.jpg' alt='index card'/ >
-
-        <p className='tagline'>inDecks: A more <span className='underline'>fun</span> and <span className='underline'>efficient</span> way to study.</p>
-      </div>
     </div>
   </div>
 );

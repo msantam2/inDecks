38b8cac1038e24d94c8dd7ea08f7dd2ab4841d2b
Matthew Santamaria <msantam2@gmail.com>
2016-09-01 17:30:42 -0700

    complete and successfully test CRUD API for cards (CardsController)

--- app/controllers/api/cards_controller.rb
+++ app/controllers/api/cards_controller.rb
@@ -1,23 +1,26 @@
 class Api::CardsController < ApplicationController
   def index
+    @cards = Card.all
   end
 
   def show
+    @card = Card.find(params[:id])
   end
 
   def create
+    @card = Card.create!(card_params)
+    render 'api/cards/show'
   end
 
-  def new
-  end
+  def destroy
+    @card = Card.find(params[:id])
 
-  def edit
-  end
-
-  def update
-  end
-
-  def destory
+    if @card.destroy
+      render 'api/cards/show', status: 200
+    else
+      @errors = @card.errors.full_messages
+      render 'api/shared/error', status: 422
+    end
   end
 
   private

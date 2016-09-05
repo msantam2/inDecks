37757673b19b36d605819b4694831a0642da6f05
Matthew Santamaria <msantam2@gmail.com>
2016-09-05 13:50:54 -0700

    more styling of flashcard component

--- frontend/components/card/card_answer_prompt.jsx
+++ frontend/components/card/card_answer_prompt.jsx
@@ -4,13 +4,13 @@ const CardAnswerPrompt = ({updateMastery}) => (
   <div>
     <p>How well did you know the answer?</p>
     <ul className='mastery-choices'>
-      <li onClick={updateMastery} className='needs-more-work-choice'>
+      <li onClick={updateMastery} className='mastery-choice-1'>
         1 Needs More Work
       </li>
-      <li onClick={updateMastery}>2</li>
-      <li onClick={updateMastery}>3</li>
-      <li onClick={updateMastery}>4</li>
-      <li onClick={updateMastery} className='mastered-choice'>
+      <li onClick={updateMastery} className='mastery-choice-2'>2</li>
+      <li onClick={updateMastery} className='mastery-choice-3'>3</li>
+      <li onClick={updateMastery} className='mastery-choice-4'>4</li>
+      <li onClick={updateMastery} className='mastery-choice-5'>
         5 Mastered!
       </li>
     </ul>

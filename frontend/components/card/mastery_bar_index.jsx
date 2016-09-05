b49fad7efccfe3f8daf15e45c7357f1e90a2a3d6
Matthew Santamaria <msantam2@gmail.com>
2016-09-05 11:32:18 -0700

    mastery progress bars working - dynamically indicating perfect of cards at each mastery level

--- frontend/components/card/mastery_bar_index.jsx
+++ frontend/components/card/mastery_bar_index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import MasteryBarIndexItem from './mastery_bar_index_item';
 
 const MasteryBarIndex = ({cards}) => {
-
+  
   let masteryBarIndexItems = [1, 2, 3, 4, 5].map(masteryLevel => (
     <MasteryBarIndexItem masteryLevel={masteryLevel} cards={cards} key={`mastery-bar-index-item-${masteryLevel}`} />
   ));
--- frontend/components/card/mastery_bar_index_item.jsx
+++ frontend/components/card/mastery_bar_index_item.jsx
@@ -2,11 +2,22 @@ import React from 'react';
 
 const MasteryBarIndexItem = ({masteryLevel, cards}) => {
   const cardKeys = Object.keys(cards);
+  const cardLength = cardKeys.length;
+  let numCardsAtMasteryLevel = 0;
+
+  cardKeys.forEach(cardKey => {
+    if (cards[cardKey].mastery === masteryLevel) {
+      numCardsAtMasteryLevel += 1;
+    }
+  });
+
+  const widthPercentage = (numCardsAtMasteryLevel / cardLength) * 100;
+  const widthStyle = {width: `${widthPercentage}%`};
 
   return (
-    <li key={`mastery-level-${masteryLevel}`} className='mastery-bar-index-item'>
+    <li key='masteryLevel' className='mastery-bar-index-item'>
       <p className='mastery-level'>{masteryLevel}</p>
-      <div className={`mastery-progress-bar-${masteryLevel} mastery-progress-bar`}></div>
+      <div style={widthStyle} className={`mastery-progress-bar-${masteryLevel} mastery-progress-bar`}></div>
     </li>
   );
 };

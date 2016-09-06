a1b4257928713ca6c26770da969d176f1b57b9d6
Matthew Santamaria <msantam2@gmail.com>
2016-09-06 11:05:45 -0700

    update delete button to delete empty row items that have not been persisted to the database but still need to be deleted. also implement + for adding an empty row to table (i.e. new card to fill out in deck)

--- frontend/components/card/card_form.jsx
+++ frontend/components/card/card_form.jsx
@@ -1,26 +1,62 @@
 import React from 'react';
 import CardFormRowItem from './card_form_row_item';
+import merge from 'lodash/merge';
 
 class CardForm extends React.Component {
   constructor(props) {
     super(props);
     this.deleteCard = this.deleteCard.bind(this);
+    this.addRow = this.addRow.bind(this);
+    this.state = {cards: {}};
   }
 
   componentDidMount () {
     this.props.requestCards();
   }
 
+  componentWillReceiveProps (newProps) {
+    if (Object.keys(newProps.cards).length > 0) {
+      this.setState({
+        cards: newProps.cards
+      });
+    }
+  }
+
   deleteCard(id) {
-    this.props.deleteCard(id);
+    let idString = id.toString();
+
+    if (Object.keys(this.props.cards).includes(idString)) {
+      this.props.deleteCard(id);
+    } else {
+      delete this.state.cards[id];
+      let newCards = this.state.cards;
+      this.setState({
+        cards: newCards
+      });
+    }
+  }
+
+  addRow() {
+    let newCard = {
+      id: Math.random(),
+      question: '',
+      answer: ''
+    };
+
+    let newCards = merge({}, this.state.cards);
+    newCards[newCard.id] = newCard;
+
+    this.setState({
+      cards: newCards
+    });
   }
 
   render() {
     let cardRows;
 
-    if (Object.keys(this.props.cards).length > 0) {
-      cardRows = Object.keys(this.props.cards).map((cardKey, idx) => (
-          <CardFormRowItem key={`card-row-${cardKey}`} idx={idx} card={this.props.cards[cardKey]} deleteCard={this.deleteCard} />
+    if (Object.keys(this.state.cards).length > 0) {
+      cardRows = Object.keys(this.state.cards).map((cardKey, idx) => (
+          <CardFormRowItem key={`card-row-${cardKey}`} idx={idx} card={this.state.cards[cardKey]} deleteCard={this.deleteCard} />
       ));
     }
 
@@ -46,7 +82,7 @@ class CardForm extends React.Component {
           <tfoot>
             <tr>
               <td>
-                <a className='add-card-plus-sign-link'>+</a>
+                <a className='add-card-plus-sign-link' onClick={this.addRow}>+</a>
               </td>
               <td className='card-edit-question'></td>
               <td className='card-edit-answer'></td>

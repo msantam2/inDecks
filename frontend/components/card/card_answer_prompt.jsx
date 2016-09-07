4276a50eea4f8dadd3b1b1e9eb78987d3cab469e
Matthew Santamaria <msantam2@gmail.com>
2016-09-06 20:31:52 -0700

    swap level 1 and 5 description (Mastery with Needs More Work)

--- frontend/components/card/card_answer_prompt.jsx
+++ frontend/components/card/card_answer_prompt.jsx
@@ -5,13 +5,13 @@ const CardAnswerPrompt = ({updateMastery}) => (
     <p className='how-well-know-answer'>How well did you know the answer?</p>
     <ul className='mastery-choices'>
       <li onClick={updateMastery} className='mastery-choice-1'>
-        1&nbsp;&nbsp;Mastered
+        1&nbsp;&nbsp;Needs More Work
       </li>
       <li onClick={updateMastery} className='mastery-choice-2'>2</li>
       <li onClick={updateMastery} className='mastery-choice-3'>3</li>
       <li onClick={updateMastery} className='mastery-choice-4'>4</li>
       <li onClick={updateMastery} className='mastery-choice-5'>
-        5&nbsp;&nbsp;Needs More Work 
+        5&nbsp;&nbsp;Mastered
       </li>
     </ul>
   </div>
